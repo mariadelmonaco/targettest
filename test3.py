f0fbc488826b32301421c921a6adad9b2dae1eeb
mariadelmonaco <mary.zmbdel@gmail.com>
2024-02-15 10:03:21 -0300

    improved code implementation

--- test3.py
+++ test3.py
@@ -4,18 +4,15 @@ with open("data.json", "r") as f:
     jsonData = json.load(f)
 
 size = 0
-for i in range(1, len(jsonData), 1):
-    fat = jsonData[i]['fat']
-    if fat != 0 and fat > 0:
-        size += 1
-print(size)
-
 total = jsonData[0]['fat']
 for i in range(1, len(jsonData), 1):
     fat = jsonData[i]['fat']
     if fat == 0 or fat > 0:
         total = total + fat
+    if fat != 0 and fat > 0:
+        size += 1
 print(total)
+print(size)
 
 average = total/size
 
@@ -26,22 +23,18 @@ for i in jsonData:
 print("total days in which revenue was greater than the monthly average:", count)
 
 low = jsonData[0]['fat']
+high = jsonData[0]['fat']
 
 for i in range(1, len(jsonData), 1):
     fat = jsonData[i]['fat']
     if (fat == 0):
         continue
-
     if (low == 0 or fat < low):
         low = fat
-
-print("Lowest billing amount occurred in the month:", low)
-
-high = jsonData[0]['fat']
-for i in range(1, len(jsonData), 1):
-    fat = jsonData[i]['fat']
-    if (fat == 0):
-        continue
+     
     if high == 0 or fat > high:
         high = fat
+print("Lowest billing amount occurred in the month:", low)
 print("Highest billing value occurred in the month:", high)
+
+
